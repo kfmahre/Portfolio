1b3584b63f6fb85ebc76bb7efb0a68562e6b600e
kfmahre <kfmahre@gmail.com>
2016-10-20 14:00:05 -0400

    added cards to the slides, lets see if that fixes the appearances

--- js/app.js
+++ js/app.js
@@ -63,10 +63,10 @@ var projects = [
 projects.forEach(function(project){
 //col-xs-12 col-sm-4 col-md-4
 //projectPic img-responsive img-rounded
-	var projectHTML = '<div class="project-slide"><a href='+project.projectUrl+
-	' "><img class="img-responsive" id="projectPic" src='+
-	project.projectImg+' alt="Picture"/><h4>'+project.name+
-	'</h4></a><p class="project-url">'+project.gitUrl+'</p></div>';
+	var projectHTML = '<div class="card text-xs-center"><a href='+project.projectUrl+
+	' "><img class="card-img-top fit" id="projectPic" src='+
+	project.projectImg+' alt="Picture"/><h4 "card-title text-thin">'+project.name+
+	'</h4></a><footer><small class="text-muted"><p class="project-url">'+project.gitUrl+'</p></small></footer></div>';
 
 	$('#projects').append(projectHTML);
 
