c28b211fcd7a85aa052c6f235e04cbe823768d4f
kfmahre <kfmahre@gmail.com>
2017-04-25 14:30:26 -0400

    style: making the animation cyclical for hover

--- js/GSanimate.js
+++ js/GSanimate.js
@@ -3,7 +3,8 @@
   TweenMax.to($('#myPicture'), 6, {ease:Bounce.easeOut, rotationY:360, transformPerspective:1600, transformOrigin:"50% 50%"});
 
   $('.hoverShadow').hover(function(){
-    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"2px 2px 0 #FAFAFA"});
+    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"2px 2px 1 #FAFAFA"});
+    TweenMax.to($(this), 0.5, {opacity:1, textShadow:"none", delay:0.5});
   });
 
 })(jQuery);
