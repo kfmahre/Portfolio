66ce6063534e3975ec7117f7e370b5801bb618b3
kfmahre <kfmahre@gmail.com>
2017-04-25 14:43:21 -0400

    style: exagerating the hover effect to better see how it's working

--- js/GSanimate.js
+++ js/GSanimate.js
@@ -3,7 +3,7 @@
   TweenMax.to($('#myPicture'), 6, {ease:Bounce.easeOut, rotationY:360, transformPerspective:1600, transformOrigin:"50% 50%"});
 
   $('.hoverShadow').hover(function(){
-    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"2px 2px 1 #FAFAFA"});
+    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"3px 3px 2 #FAFAFA"});
     TweenMax.to($(this), 0.5, {opacity:1, textShadow:"0px 0px 0 #FAFAFA", delay:0.5});
   });
 
