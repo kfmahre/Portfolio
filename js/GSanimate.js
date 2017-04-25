8074997d783d257e6d14b08e385d09b79cb188c9
kfmahre <kfmahre@gmail.com>
2017-04-25 14:48:45 -0400

    trying again

--- js/GSanimate.js
+++ js/GSanimate.js
@@ -3,8 +3,8 @@
   TweenMax.to($('#myPicture'), 6, {ease:Bounce.easeOut, rotationY:360, transformPerspective:1600, transformOrigin:"50% 50%"});
 
   $('.hoverShadow').hover(function(){
-    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"3px 3px 2 #FAFAFA"});
-    TweenMax.to($(this), 0.5, {opacity:1, textShadow:"0px 0px 0 #FAFAFA", delay:0.5});
+    TweenMax.to($(this), 0.5, {opacity:.5, textShadow:"0px 0px 2px #FAFAFA"});
+    TweenMax.to($(this), 0.5, {opacity:1, textShadow:"0px 0px 0px #FAFAFA", delay:0.5});
   });
 
 })(jQuery);
