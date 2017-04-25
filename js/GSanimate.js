3da9642901cee73216461da215bfdb5e4de318df
kfmahre <kfmahre@gmail.com>
2017-04-25 14:19:34 -0400

    feat: adding hover tween

--- js/GSanimate.js
+++ js/GSanimate.js
@@ -2,4 +2,8 @@
 
   TweenMax.to($('#myPicture'), 6, {ease:Bounce.easeOut, rotationY:360, transformPerspective:1600, transformOrigin:"50% 50%"});
 
+  $('.hoverShadow').hover(function(){
+    TweenMax.to($(this), 0.5, {opacity:.9, textShadow:"2px 2px 0 #FAFAFA"});
+  });
+
 })(jQuery);
