1d76a0634c51f1ea9dd0a714fe39410d30599964
kfmahre <kfmahre@gmail.com>
2018-03-13 14:46:56 -0400

    removed some unused files

--- js/GSanimate.js
+++ js/GSanimate.js
@@ -3,7 +3,7 @@
   TweenMax.to($('#myPicture'), 6, {ease:Bounce.easeOut, rotationY:360, transformPerspective:1600, transformOrigin:"50% 50%"});
 
   $('.hoverShadow').hover(function(){
-    TweenMax.to($(this), 0.5, {opacity:.5, textShadow:"0px 0px 2px #FAFAFA"});
+    TweenMax.to($(this), 0.5, {opacity:.5, textShadow:"0px 0px 5px #FAFAFA"});
     TweenMax.to($(this), 0.5, {opacity:1, textShadow:"0px 0px 0px #FAFAFA", delay:0.5});
   });
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Portfolio Readme
 
 ### I started this project as a part of code coursework for my front-end web developer nanodegree with Udacity
-### Since completion I've completely revamped the whole thing, using a jekyll theme I have customized heavily.
+### Since completion I've completely revamped the whole thing, using jekyll to display some of my projects.
 
 ## Project active @: http://kfmahre.github.io/Portfolio/
